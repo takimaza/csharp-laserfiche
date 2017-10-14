ad62e59ed42ebc9d85876f3a1ed0907c9ae5396e
Eduardo Montalvo <eduardo.montalvo@kibrio.com>
2017-10-14 03:10:25 -0500

    Initial commit

---
+++ LaserficheCGA/formPrincipal.Designer.cs
@@ -0,0 +1,194 @@
+﻿namespace LaserficheCGA
+{
+    partial class formPrincipal
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.gridResultado = new System.Windows.Forms.DataGridView();
+            this.cmbFechaInicial = new System.Windows.Forms.DateTimePicker();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.cmbFechaFinal = new System.Windows.Forms.DateTimePicker();
+            this.btnCancelar = new System.Windows.Forms.Button();
+            this.btnBuscar = new System.Windows.Forms.Button();
+            this.btnProcesar = new System.Windows.Forms.Button();
+            this.cmbOficinas = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.gridResultado)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // gridResultado
+            // 
+            this.gridResultado.AllowUserToAddRows = false;
+            this.gridResultado.AllowUserToDeleteRows = false;
+            this.gridResultado.AllowUserToOrderColumns = true;
+            this.gridResultado.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+                        | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.gridResultado.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.gridResultado.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.gridResultado.Location = new System.Drawing.Point(12, 34);
+            this.gridResultado.Name = "gridResultado";
+            this.gridResultado.ReadOnly = true;
+            this.gridResultado.Size = new System.Drawing.Size(796, 240);
+            this.gridResultado.TabIndex = 6;
+            // 
+            // cmbFechaInicial
+            // 
+            this.cmbFechaInicial.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.cmbFechaInicial.Location = new System.Drawing.Point(421, 8);
+            this.cmbFechaInicial.Name = "cmbFechaInicial";
+            this.cmbFechaInicial.Size = new System.Drawing.Size(91, 20);
+            this.cmbFechaInicial.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(331, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(84, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Fecha Inicial:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(516, 12);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(77, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Fecha Final:";
+            // 
+            // cmbFechaFinal
+            // 
+            this.cmbFechaFinal.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.cmbFechaFinal.Location = new System.Drawing.Point(599, 8);
+            this.cmbFechaFinal.Name = "cmbFechaFinal";
+            this.cmbFechaFinal.Size = new System.Drawing.Size(91, 20);
+            this.cmbFechaFinal.TabIndex = 1;
+            // 
+            // btnCancelar
+            // 
+            this.btnCancelar.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnCancelar.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.btnCancelar.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCancelar.Location = new System.Drawing.Point(650, 283);
+            this.btnCancelar.Name = "btnCancelar";
+            this.btnCancelar.Size = new System.Drawing.Size(75, 23);
+            this.btnCancelar.TabIndex = 5;
+            this.btnCancelar.Text = "Cancelar";
+            this.btnCancelar.UseVisualStyleBackColor = true;
+            this.btnCancelar.Click += new System.EventHandler(this.btnCancelar_Click);
+            // 
+            // btnBuscar
+            // 
+            this.btnBuscar.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnBuscar.Location = new System.Drawing.Point(696, 6);
+            this.btnBuscar.Name = "btnBuscar";
+            this.btnBuscar.Size = new System.Drawing.Size(116, 23);
+            this.btnBuscar.TabIndex = 3;
+            this.btnBuscar.Text = "Buscar";
+            this.btnBuscar.UseVisualStyleBackColor = true;
+            this.btnBuscar.Click += new System.EventHandler(this.btnBuscar_Click);
+            // 
+            // btnProcesar
+            // 
+            this.btnProcesar.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnProcesar.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnProcesar.Location = new System.Drawing.Point(731, 283);
+            this.btnProcesar.Name = "btnProcesar";
+            this.btnProcesar.Size = new System.Drawing.Size(75, 23);
+            this.btnProcesar.TabIndex = 4;
+            this.btnProcesar.Text = "Procesar";
+            this.btnProcesar.UseVisualStyleBackColor = true;
+            this.btnProcesar.Click += new System.EventHandler(this.btnProcesar_Click);
+            // 
+            // cmbOficinas
+            // 
+            this.cmbOficinas.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmbOficinas.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cmbOficinas.FormattingEnabled = true;
+            this.cmbOficinas.Location = new System.Drawing.Point(69, 7);
+            this.cmbOficinas.Name = "cmbOficinas";
+            this.cmbOficinas.Size = new System.Drawing.Size(256, 21);
+            this.cmbOficinas.TabIndex = 7;
+            this.cmbOficinas.SelectedIndexChanged += new System.EventHandler(this.cmbOficinas_SelectedIndexChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(12, 10);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(51, 13);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Oficina:";
+            // 
+            // formPrincipal
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.CancelButton = this.btnCancelar;
+            this.ClientSize = new System.Drawing.Size(818, 312);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.cmbOficinas);
+            this.Controls.Add(this.btnProcesar);
+            this.Controls.Add(this.btnBuscar);
+            this.Controls.Add(this.btnCancelar);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.cmbFechaFinal);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.cmbFechaInicial);
+            this.Controls.Add(this.gridResultado);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "formPrincipal";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Documentación Laserfiche | D.A. Hinojosa";
+            this.Load += new System.EventHandler(this.formPrincipal_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.gridResultado)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView gridResultado;
+        private System.Windows.Forms.DateTimePicker cmbFechaInicial;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.DateTimePicker cmbFechaFinal;
+        private System.Windows.Forms.Button btnCancelar;
+        private System.Windows.Forms.Button btnBuscar;
+        private System.Windows.Forms.Button btnProcesar;
+        private System.Windows.Forms.ComboBox cmbOficinas;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ LaserficheCGA/formPrincipal.cs
@@ -0,0 +1,245 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+using System.Data.Odbc;
+using System.Data.OleDb;
+
+using System.Data.SqlClient;
+using System.IO;
+
+using LFSO81Lib;
+using DocumentProcessor81;
+using PdfExporter81;
+
+namespace LaserficheCGA
+{
+    public partial class formPrincipal : Form
+    {
+        private LFConnection conn;
+        private LFDatabase db;                
+        private LFServer server;
+
+        private string foxpro_db;
+        private string sqlserver_db;
+        private string sqlserver_sp;
+
+        public formPrincipal() 
+        {
+            InitializeComponent();
+        }
+
+        private void formPrincipal_Load(object sender, EventArgs e)
+        {
+            this.conn = new LFConnection();
+            this.server = new LFApplication().GetServerByName("server_lf");
+            this.db = server.GetDatabaseByName("DA-HINOJOSA");
+            this.conn.UserName = "pablo.montalvo";
+            this.conn.Password = "P123456";
+            this.conn.Create(this.db);
+
+            this.cargarCombos();
+            this.cmbOficinas.SelectedIndex = 0;
+        }
+        
+        private void cargarCombos()
+        {
+            this.cmbOficinas.Items.Add("AGENCIA ADUANAL");
+            this.cmbOficinas.Items.Add("AGENCIA ADUANAL - ARCHIVOS S.F."); // Archivos sin folio fiscal
+            this.cmbOficinas.Items.Add("FUMIT");
+            this.cmbOficinas.Items.Add("TERMINAL");
+            this.cmbOficinas.Items.Add("INTERFLET");
+        }   
+
+        private void cmbOficinas_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            int seleccion = cmbOficinas.SelectedIndex;
+            switch (seleccion)
+            {
+                case 0:
+                    this.foxpro_db = "datrefer";
+                    this.sqlserver_db = "1G_DAH_AA";
+                    this.sqlserver_sp = "UP_Consulta_ArchivosAgencia";
+                    break;
+                case 1:
+                    this.foxpro_db = "datrefer";
+                    this.sqlserver_db = "1G_DAH_AA";
+                    this.sqlserver_sp = "UP_Consulta_ArchivosAgencia_SF";
+                    break;
+                case 2:
+                    this.foxpro_db = "fumit";
+                    this.sqlserver_db = "1G_DAH_FUMIT";
+                    this.sqlserver_sp = "UP_Consulta_ArchivosFumit";
+                    break;
+                case 3:
+                    this.foxpro_db = "terminal";
+                    this.sqlserver_db = "1G_DAH_TERMINAL";
+                    this.sqlserver_sp = "UP_Consulta_ArchivosTerminal";
+                    break;
+                case 4:
+                    this.foxpro_db = "interflet";
+                    this.sqlserver_db = "PENDIENTE";
+                    this.sqlserver_sp = "PENDIENTE";
+                    break;
+            }
+        }
+
+        private void btnBuscar_Click(object sender, EventArgs e)
+        {
+            Cursor = Cursors.WaitCursor;
+            string fechaInicial = this.cmbFechaInicial.Value.ToString("MM/dd/yyyy");
+            string fechaFinal = this.cmbFechaFinal.Value.ToString("MM/dd/yyyy");
+            try
+            {
+                this.gridResultado.DataSource = this.getData(fechaInicial, fechaFinal).Tables[0];               
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                Cursor = Cursors.Arrow;
+            }
+        }
+
+        private DataSet getData(string fechaInicial, string fechaFinal)
+        {
+            using (SqlConnection conn = this.getConnectionSQL())
+            {
+                using (SqlCommand command = new SqlCommand(this.sqlserver_sp, conn))
+                { 
+                    command.CommandType = CommandType.StoredProcedure;
+                    conn.Open();
+                    
+                    command.Parameters.AddWithValue("@FechaIni", fechaInicial);
+                    command.Parameters.AddWithValue("@FechaFin", fechaFinal);
+
+                    using (SqlDataAdapter adapter = new SqlDataAdapter(command))
+                    {
+                        DataSet data = new DataSet();
+                        adapter.Fill(data);
+                        return data;
+                    }
+                }
+            }
+        }
+
+        private SqlConnection getConnectionSQL()
+        {
+            return new SqlConnection("Integrated Security=False;User ID=sa;Password=sa;Initial Catalog="+this.sqlserver_db+";Data Source=175.50.5.3");
+        }  
+
+        private void btnProcesar_Click(object sender, EventArgs e)
+        {
+            Cursor = Cursors.WaitCursor;
+            if (this.gridResultado.Rows.Count > 0)
+            {
+                this.deleteFoxProData();
+               
+                foreach (DataGridViewRow row in this.gridResultado.Rows)
+                {
+                    string archivo, cliente, referencia, movimiento, numero, serie, anio, mes, ruta, folio;
+                    archivo = row.Cells[0].Value.ToString();
+                    cliente = row.Cells[1].Value.ToString();
+                    referencia = row.Cells[2].Value.ToString();
+                    movimiento = row.Cells[3].Value.ToString();
+                    numero = row.Cells[4].Value.ToString();
+                    serie = row.Cells[5].Value.ToString();
+                    anio = row.Cells[6].Value.ToString();
+                    mes = row.Cells[7].Value.ToString();
+                    ruta = getRuta(referencia);
+                    folio = "6000";
+
+                    this.insertFoxProData(archivo, cliente, referencia, ruta, movimiento, numero, serie, folio); 
+                   
+                   // this.moveFile(archivo);                    
+                }                
+            }
+            MessageBox.Show("Proceso terminado");
+            Cursor = Cursors.Arrow;
+        }
+
+        private void deleteFoxProData()
+        {
+            this.actionFoxPro("DELETE FROM " + this.foxpro_db);
+        }
+
+        private string getRuta(string referencia)
+        {
+
+            string ruta = "SIN RUTA EN LF";
+            try
+            {
+                LFSearch busqueda = this.db.CreateSearch();
+                string queryLF = "{LF:ParentName=\"" + referencia + "\"}";
+                busqueda.Command = queryLF;
+                busqueda.BeginSearch(true);
+                ILFCollection archivos = busqueda.GetSearchHits();
+                if (archivos.Count > 0)
+                {
+                    LFSearchHit hit = (LFSearchHit)archivos[1];
+                    ILFEntry entry = (ILFEntry)hit.Entry;
+                    LFFolder folder = entry.ParentFolder;
+                    ruta = folder.FullPath + @"\";                    
+                }
+                busqueda.Dispose();
+                return ruta;
+            }
+
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+                return ruta;
+            }
+        }
+
+        private void insertFoxProData(string archivo, string cliente, string referencia, string ruta, string movimiento, string numero, string serie, string folio)
+        {
+            this.actionFoxPro("INSERT INTO " + this.foxpro_db + " (archivo, Cliente, referencia, Ruta, tipomov, numero, serie, folio) VALUES ('" + archivo + "'," + cliente + ",'" + referencia + "','" + ruta + "',1," + numero + ",'" + serie + "','" + serie + "' )");
+        }
+
+        private void actionFoxPro(string action)
+        {
+            string path = @"h:\sica\util\scontrol\";
+
+            if (Directory.Exists(path))
+            {
+                using (OdbcConnection con = new OdbcConnection("Driver={Microsoft Visual FoxPro Driver};SourceType=DBF;SourceDB=" + path))
+                {
+                    con.Open();
+                    using (OdbcCommand command = new OdbcCommand(action, con))
+                    {
+                        command.ExecuteNonQuery();                        
+                    }
+                }
+            }
+            else
+            {
+                MessageBox.Show("¡LA UBICACIÓN: " + path + " NO EXISTE!");
+            }
+        }
+
+        private void btnCancelar_Click(object sender, EventArgs e)
+        {
+            conn.Terminate();
+            Application.Exit();
+        }
+
+        private void moveFile(string archivo)
+        {
+            if (File.Exists(@"Z:\OneGoalCFD\XML_AA\" + archivo + ".pdf") && File.Exists(@"Z:\OneGoalCFD\XML_AA\" + archivo + ".xml"))
+            {
+                if (Directory.Exists(@"C:\Andres\BORRAR\XML_AA\"))
+                {
+                    File.Copy(@"Z:\OneGoalCFD\XML_AA\" + archivo + ".pdf", @"C:\Andres\BORRAR\XML_AA\" + archivo + ".pdf");
+                    File.Copy(@"Z:\OneGoalCFD\XML_AA\" + archivo + ".xml", @"C:\Andres\BORRAR\XML_AA\" + archivo + ".xml");
+                }
+            }
+        }
+    }
+}
